a4609a79ecdac1bbf5b17d52be3c53c3b2995893
AriyanMlzm <ryanmolazem@gmail.com>
2024-05-03 22:15:30 +0330

    update log formula

--- src/helpers/convertLog.js
+++ src/helpers/convertLog.js
@@ -2,7 +2,7 @@ const convertLog = (image, c, w, h) => {
   for (let row = 0; row < h; row++) {
     for (let col = 0; col < w; col++) {
       var pixelValue = image.ucharPtr(row, col)[0]
-      image.ucharPtr(row, col)[0] = c * Math.log10(1 + pixelValue)
+      image.ucharPtr(row, col)[0] = c * Math.log(1 + pixelValue)
     }
   }
 }
